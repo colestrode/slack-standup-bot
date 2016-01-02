66b2afe7727fb10e4313b623fde09dec821c5845
Cole Furfaro-Strode <colestrode@gmail.com>
2016-01-02 16:26:07 -0500

    improved readme, started on summary controller test

---
+++ test/controller/summary-controller.spec.js
@@ -0,0 +1,17 @@
+var chai = require('chai')
+  , expect = chai.expect
+  , sinon = require('sinon')
+  , proxyquire = require('proxyquire');
+
+chai.use(require('sinon-chai'));
+
+describe('Summary Controller', function() {
+
+  beforeEach(function() {
+
+  });
+
+  it('should ', function() {
+
+  });
+});
